90614afd5ef75d11cf1c2e8d4dd19812e623b4f2
Facupitta <facupitta@hotmail.com>
2025-03-09 19:38:09 -0300

    fix rutas

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -8,8 +8,8 @@ router.get('/', list)
   .get('/:id', find)
   .post('/', authMiddleware, add)
   .put('/:id', authMiddleware, update)
-  .post(':id/recommendations', authMiddleware, like)
-  .post(':id/purchases', authMiddleware, purchase)
-  .post(':id/comment', authMiddleware, comment);
+  .post('/:id/recommendations', authMiddleware, like)
+  .post('/:id/purchases', authMiddleware, purchase)
+  .post('/:id/comment', authMiddleware, comment);
 
 module.exports = router;
