f7cd6d74900ad90f298f85dffb1f248daea3daea
Valentín <valentinc1515@gmail.com>
2025-03-10 02:20:14 -0300

    update final

--- Backend/database.js
+++ Backend/database.js
@@ -1,8 +1,9 @@
 const { Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize("todotech", "todotech", "t0d0t3ch", {
-  host: "localhost",
-  dialect: "mysql",
+const sequelize = new Sequelize("todotech_supperhot", "todotech_supperhot", "5671f10100fd47f54fb2c05b522ff2ed7ce6eab3", {
+  host: "z2d7a.h.filess.io",
+  dialect: "mysql", 
+  port: 3307
 });
 
 module.exports = sequelize;
